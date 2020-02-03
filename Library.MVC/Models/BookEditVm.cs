4c8b1c24f5d8aa969d07f08738b69c6e0f419a2e
Dreeoo <55452061+Dreeoo@users.noreply.github.com>
2020-02-03 13:42:28 +0100

    Edit?

---
+++ Library.MVC/Models/BookEditVm.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.MVC.Models
+{
+    public class BookEditVm
+    {
+        [Required]
+        public int ID { get; set; }
+        public string ISBN { get; set; }
+        [Display(Name = "Titel")]
+        [MaxLength(7)]
+        public string Title { get; set; }
+        [Display(Name = "Författare")]
+        public int AuthorID { get; set; }
+        public int AuthorId { get; set; }
+        public string Description { get; set; }
+    }
+}
