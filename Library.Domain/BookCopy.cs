152dc3188fd5f008cf5de7f2a8929e58debf0c1c
Dreeoo <55452061+Dreeoo@users.noreply.github.com>
2020-02-11 11:59:03 +0100

    Revert "Test av DB update"
    This reverts commit 47fc8912be211f653348201e066016725adc8dfa.

--- Library.Domain/BookCopy.cs
+++ Library.Domain/BookCopy.cs
@@ -1,11 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Library.Domain
+﻿namespace Library.Domain
 {
     public class BookCopy
     {
         public int ID { get; set; }
-        [ForeignKey("Loan")]
         public int LoanID { get; set; }
         public BookDetails Details { get; set; }
 
