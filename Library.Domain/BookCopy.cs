47fc8912be211f653348201e066016725adc8dfa
Dreeoo <55452061+Dreeoo@users.noreply.github.com>
2020-02-11 11:08:15 +0100

    Test av DB update

--- Library.Domain/BookCopy.cs
+++ Library.Domain/BookCopy.cs
@@ -1,8 +1,11 @@
-﻿namespace Library.Domain
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Domain
 {
     public class BookCopy
     {
         public int ID { get; set; }
+        [ForeignKey("Loan")]
         public int LoanID { get; set; }
         public BookDetails Details { get; set; }
 
