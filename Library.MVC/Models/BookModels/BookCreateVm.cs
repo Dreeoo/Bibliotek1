8b686ab07b2887f88757f7d8f48599ea2ca6009e
AlexEllner <alex.ellner@live.com>
2020-02-24 16:44:06 +0100

    BookCreateVm validation

--- Library.MVC/Models/BookModels/BookCreateVm.cs
+++ Library.MVC/Models/BookModels/BookCreateVm.cs
@@ -10,14 +10,34 @@ namespace Library.MVC.Models
     public class BookCreateVm
     {
         [Required]
+        [Display(Name = "ISBN")]
+        [DataType(DataType.Text)]
+        /*[RegularExpression(@"^(?:ISBN(?:-1[03])?:?●)?(?=[0-9X]{10}$|
+(?=(?:[0-9]+[-●]){3})[-●0-9X]{13}$|
+97[89][0-9]{10}$|(?=(?:[0-9]+[-●]){4})[-●0-9]{17}$)|
+(?:97[89][-●]?)?[0-9]{1,5}[-●]?[0-9]+[-●]?[0-9]+[-●]?[0-9X]$", ErrorMessage = "Please enter a valid ISBN")]*/
         public string ISBN { get; set; }
-        [Display(Name = "Titel")]
-        [MaxLength(7)]
+
+        [Required]
+        [Display(Name = "Book Title")]
+        [DataType(DataType.Text)]
+        [StringLength(50, MinimumLength = 1)]
         public string Title { get; set; }
-        [Display(Name = "Författare")]
+
+        [Required]
+        [Display(Name = "Author Name")]
         public SelectList AuthorList { get; set; }
+
         public int AuthorID { get; set; }
+
+        [Required]
+        [Display(Name = "Book Description")]
+        [DataType(DataType.Text)]
+        [StringLength(500, MinimumLength = 4)]
         public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Book Copies")]
         public int Copies { get; set; }
 
     }
